b3f940c247e64dcea57ead17b3be554ae5edc080
Fatemeh Ahmad <Ahmadzadeh920@users.noreply.github.com>
2024-12-07 23:50:17 -0800

    Set up project

---
+++ core/Core/settings/development.py
@@ -0,0 +1,14 @@
+
+from .common import *
+
+
+# Email
+domain = os.getenv("Domain")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp4dev"
+EMAIL_PORT = "25"
+EMAIL_USE_TLS = False
+
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+
