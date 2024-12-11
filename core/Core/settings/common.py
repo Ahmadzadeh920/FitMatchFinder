069eeb8314c57ebb0584cd3b4af89812c263526f
Fatemeh Ahmad <Ahmadzadeh920@users.noreply.github.com>
2024-12-10 23:13:36 -0800

    resolve the bug
    bug related to connection of postgre and backend

--- core/Core/settings/common.py
+++ core/Core/settings/common.py
@@ -109,7 +109,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': 'as@1234',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'db',  # This should match the service name in docker-compose
         'PORT': '5432',
     }
