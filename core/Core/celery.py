f66763f8c52383ad0170752759599249bf0cb956
Fatemeh Ahmad <Ahmadzadeh920@users.noreply.github.com>
2024-12-18 05:16:46 -0800

    task.py

--- core/Core/celery.py
+++ core/Core/celery.py
@@ -2,9 +2,13 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Core.settings.common")
 
 app = Celery("Core")
-# Load task modules from all registered Django app configs.
-app.config_from_object("django.conf:settings", namespace="CELERY")
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings.common', namespace='CELERY')
+
+# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
--- core/Core/settings/common.py
+++ core/Core/settings/common.py
@@ -170,6 +170,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CELERY_BROKER_URL = "redis://redis:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_BACKEND= "redis://redis:6379/0"
+CELERY_TIME_ZONE = "Asia/Tehran"
 
 
 # Caching configuration
